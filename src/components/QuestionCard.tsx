2b9265a03641d9728b371e11c75541ca084fa15f
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-04-07 08:36:58 +0200

    Adds quizz implementation

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p>
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <div key={answer}>
+            <button
+              value={answer}
+              disabled={userAnswer ? true : false}
+              onClick={callback}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
